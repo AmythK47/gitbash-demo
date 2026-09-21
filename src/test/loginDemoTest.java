e6e4dd1dffee6668335809826580da017c20727a
Amit <amitkatti5@gmail.com>
2026-09-21 11:03:05 +0530

    Developer A changes to login

--- src/test/loginDemoTest.java
+++ src/test/loginDemoTest.java
@@ -1,7 +1,7 @@
 public class LoginTest {
 
     public void login() {
-        System.out.println("Login with admin credentials");
+        System.out.println("Login with admin account");
     }
 
     public void logout() {
